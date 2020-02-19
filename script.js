bfaf32cd6b44a17ab48aa45990f38c7987ce379b
markseva000 <markseva000@warren.k12.in.us>
2020-02-19 11:22:36 -0500

    workin

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 let score = 0;
-let result = " ";
 
-let btn0 = document.getElementById("btn0");
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
+let pie = document.getElementById("questNum"); pie.innerText = "4";
+
+btn0 = document.getElementById("btn0");
 
 
 
 
-
-
-let createQuestion = function (questionText, choice0, choice1, choice2, choice3) {
+let createQuestion = function (questionText, choice0, choice1, choice2, choice3 , rightChoice) {
 
     let question = document.getElementById("question");
     question.innerText = questionText;
@@ -28,19 +24,44 @@ let createQuestion = function (questionText, choice0, choice1, choice2, choice3)
     answer2.innerText = choice2;
 
 
-    let answer3 = document.getElementById("choice3");
+     let answer3 = document.getElementById("choice3");
     answer3.innerText = choice3;
 
+    rightChoice = document.getElementById("rightChoice");
+
 };
 
-while(score >= 4){
-     createQuestion("What is this project for?", "To test my skills", "Busy Work", "So the boss man won't fire Law", "Cash money baby!");
-     if(btn3.onclick){
-         score++;
-     }else {
-         score--;
-     }
+let bumpQ = function (Number){
+    let num = document.getElementById("progress");
+
+    num.innerText = Number;
 };
-alert(score);
+
+
+    let Quest1 = function() {
+
+    createQuestion("who" , "what", "where", "how", "huh", "maybe");
+
+    bumpQ("1");
+
+    btn0.onclick = Quest2();
+
+};
+
+let Quest2 = function() {
+
+    createQuestion("o" , "wt", "wre", "hw", "h", "mbe");
+
+    bumpQ("2");
+
+};
+
+
+
+
+
+
+
+
 
 
